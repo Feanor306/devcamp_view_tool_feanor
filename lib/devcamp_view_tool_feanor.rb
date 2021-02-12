ba57535eb2cdfa7acf36089f5916f93cc23a2994
Feanor306 <feanor306@live.com>
2021-02-12 13:26:24 +0100

    Dev tool init commit

---
+++ lib/devcamp_view_tool_feanor.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "devcamp_view_tool_feanor/version"
+require_relative "devcamp_view_tool_feanor/renderer"
+
+module DevcampViewToolFeanor
+  class Error < StandardError; end
+  # Your code goes here...
+end
