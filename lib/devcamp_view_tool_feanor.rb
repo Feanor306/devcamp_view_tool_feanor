7d224b7329eb4ead34ee70d756ce05b6b55ec2bb
Feanor306 <feanor306@live.com>
2021-02-12 13:50:37 +0100

    double definition fix

--- lib/devcamp_view_tool_feanor.rb
+++ lib/devcamp_view_tool_feanor.rb
@@ -3,7 +3,4 @@
 require_relative "devcamp_view_tool_feanor/version"
 require_relative "devcamp_view_tool_feanor/renderer"
 
-module DevcampViewToolFeanor
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
